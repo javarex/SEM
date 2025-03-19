443dfc19421fd27e3a1ce66fcad18e0b8277e1b4
javarex <raymartnayreitanong11@gmail.com>
2025-03-19 17:38:18 +0800

    update

--- app/Filament/Resources/StudentResource/Pages/ListStudents.php
+++ app/Filament/Resources/StudentResource/Pages/ListStudents.php
@@ -16,7 +16,7 @@ class ListStudents extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-//            Actions\CreateAction::make(),
+            Actions\CreateAction::make(),
             \EightyNine\ExcelImport\ExcelImportAction::make()
                 ->slideOver()
                 ->color("primary")
